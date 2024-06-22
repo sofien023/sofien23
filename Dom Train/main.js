b67be512abc3c39eef07a6a2d2df0a03502be2a6
sofien023 <125787995+sofien023@users.noreply.github.com>
2024-06-22 18:37:29 +0100

    success

---
+++ Dom Train/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const element = document.createElement('p');
+    element.textContent = "Hello, World";
+    document.body.appendChild(element);
+});
